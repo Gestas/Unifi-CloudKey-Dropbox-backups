cead17d697c21b15cf0480dbdb1ece3e7a12a6e5
Gestas <craig@gestas.net>
2019-12-10 14:15:22 -0800

    initial commit

---
+++ CloudKey-Dropbox-backups.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+### This should be your Dropbox token - 
+readonly TOKEN=""
+
+### Optionally change these - 
+readonly DEST_DIR="/backups"
+readonly SRC_DIR="/data/autobackup"
+readonly LOG_FILE="$SRC_DIR/dropbox-upload.log"
+
+log() {
+    local _now
+    local _message
+
+    _now="$(date --rfc-3339=seconds)"
+    _message="$1"
+    printf "%s\n" "$_now: $_message" >> "$LOG_FILE"
+}
+
+upload_backups() {
+    local _f
+    local _response_code
+
+    for _f in "$SRC_DIR"/*.unf; do
+        _response_code="$(do_upload "$_f")"
+        if [[ "$_response_code" -eq "200" ]]; then
+            log "Backup of $_f uploaded to Dropbox."
+        else 
+            log "Backup $_f failed to upload to Dropbox. Response code $_response_code"
+            exit 1
+        fi
+    done
+}
+
+upload_log(){
+    local _response_code
+
+    _response_code="$(do_upload "$LOG_FILE")"
+    if [[ "$_response_code" -ne "200" ]]; then
+        printf "%s\n" "$_now: Unable to upload the log. Response code $_response_code"
+        exit 1
+    fi
+
+}
+
+do_upload() {
+    local _f
+    local _filename
+    local _response_code
+
+    _f=$1
+
+    _filename="$(/usr/bin/basename "$_f")"
+    _response_code=$(/usr/bin/curl -X POST -sL -w "%{http_code}" --output /dev/null https://content.dropboxapi.com/2/files/upload \
+        --header "Authorization: Bearer $TOKEN" \
+        --header "Dropbox-API-Arg: {\"path\": \"$DEST_DIR/$_filename\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}" \
+        --header "Content-Type: application/octet-stream" \
+        --data-binary @$_f)
+    echo "$_response_code"
+}
+
+main() {
+    log "Starting $0"
+    printf "%s\n" "Starting $0"
+    upload_backups
+    log "Uploading backups complete."
+    /sbin/fsync "$LOG_FILE"
+    log "$0 done."
+    upload_log
+    printf "%s\n" "$0 complete"
+    exit 0
+}
+
+main "$@"
---
+++ README.md
@@ -0,0 +1,40 @@
+## Unifi CloudKey backups
+Automatically upload your Unifi CloudKey backups to Dropbox.
+
+USAGE:
+
+  1. Create a Dropbox app ("Unifi CloudKey") at https://www.dropbox.com/developers/apps/create.
+  2. Create a folder ("backups") for your app. 
+    * You can get the curl command, including the token, at https://www.dropbox.com/developers/documentation/http/documentation#files-create_folder
+    * Update it with the path to the backup folder. It should look alot like this - 
+```
+$ curl -X POST https://api.dropboxapi.com/2/files/create_folder_v2 \
+--header "Authorization: Bearer XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" \
+--header "Content-Type: application/json" \
+--data "{\"path\": \"/backups\",\"autorename\": false}"
+```
+    * Run it on the CloudKey. The response should look like this - 
+```
+{"metadata": {"name": "backups", "path_lower": "/backups", "path_display": "/backups", "id": "id:YYYYYYYYYYYYYYY"}}
+```
+  3. Get and chmod the backup script -
+```
+$ wget -O /usr/bin/CloudKey-Dropbox-backups.sh \
+https://raw.githubusercontent.com/Gestas/Unifi-CloudKey-Dropbox-backups/master/CloudKey-Dropbox-backups.sh $$ \
+chmod +x /usr/bin/Pi-Hole_Cloudflare_TLS.sh
+```
+  4. Update the script with your Dropbox token (from the earlier folder creation step) -
+```
+vi /usr/bin/Pi-Hole_Cloudflare_TLS.sh
+```
+  5. Add a task to cron. My CloudKey creates a backup everyday at 4PM so I run this upload script at 5 -
+  ```
+  $ crontab -e
+  # Add - 
+  0 17 * * * "/usr/bin/CloudKey-Dropbox-backups.sh"
+  ```
+
+You can run the script manually to make sure it works - 
+```
+$ /usr/bin/CloudKey-Dropbox-backups.sh
+```
